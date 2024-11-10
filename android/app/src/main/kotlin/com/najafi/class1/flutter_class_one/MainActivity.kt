a97498ad47818390430a4f6b9583381de2b02091
Najafi <rashednajafi@yahoo.com>
2024-11-10 23:11:50 +0600

    Exam Week 2

---
+++ android/app/src/main/kotlin/com/najafi/class1/flutter_class_one/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.najafi.class1.flutter_class_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+//import 'package:flutter/material.dart';
+
+//void main() {
+  //runApp(YaAliMadadApp());
+//}
+
+//class YaAliMadadApp extends StatelessWidget {
+  //@override
+  //Widget build(BuildContext context) {
+    //return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      //title: 'Bangla Duas',
+      //home: Scaffold(
+        //backgroundColor: Colors.lightGreenAccent,
+        //appBar: AppBar(
+          //title: Text('Bangla Duas'),
+          //centerTitle: true,
+          //backgroundColor: Colors.white,
+        //),
+        //body: Image.asset('images/hussain_as.jpg')
+      //),
+    //);
+  //}
+//}
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ExamWeek2Assign());
+}
+
+class ExamWeek2Assign extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ExamWeek2Dashboard(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class ExamWeek2Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orangeAccent,
+        title: Text("Exam Week 2 Profile"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.phone),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InfoCard(
+              icon: Icons.icecream,
+              text: "Ice cream is very delicious, isn't it?",
+            ),
+            SizedBox(height: 25),
+            InfoCard(
+              icon: Icons.code,
+              text: "Programming is enjoyable if you're passionate about it!",
+            ),
+            SizedBox(height: 25),
+            InfoCard(
+              icon: Icons.egg,
+              text: "Submitting code directly copied will result in a zero mark.",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  InfoCard({required this.icon, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 48,
+          backgroundColor: Colors.purple[100],
+          child: Icon(
+            icon,
+            size: 44,
+            color: Colors.deepPurple,
+          ),
+        ),
+        SizedBox(height: 8),
+        Text(
+          text,
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
+
